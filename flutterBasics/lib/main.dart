c4040b71d49a13b074f7a9113db94ba2aee28a86
lmcevoy <lmcevoy@stevens.edu>
2020-08-07 13:09:39 -0400

    Connecting buttons to functions

--- flutterBasics/lib/main.dart
+++ flutterBasics/lib/main.dart
@@ -8,14 +8,38 @@ void main() {
 void main() => runApp(MyApp()); */
 
 class MyApp extends StatelessWidget {
+  void answerQuestion() {
+    print('Answer chosen!');
+  }
+
   @override
   Widget build(BuildContext context) {
+    var questions = [
+      'What\'s your favorite color?',
+      'What\'s your favorite animal'
+    ];
     return MaterialApp(
       home: Scaffold(
         appBar: AppBar(
           title: Text('My First App'),
         ),
-        body: Text('Hello, World'),
+        body: Column(
+          children: [
+            Text('The question'),
+            RaisedButton(
+              child: Text('Answer 1'),
+              onPressed: answerQuestion,
+            ),
+            RaisedButton(
+              child: Text('Answer 2'),
+              onPressed: answerQuestion,
+            ),
+            RaisedButton(
+              child: Text('Answer 3'),
+              onPressed: answerQuestion,
+            ),
+          ],
+        ),
       ),
     );
   }

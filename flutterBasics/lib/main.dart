38015d55f6bbf9ae5213a12a8afb9b18728c5862
lmcevoy <lmcevoy@stevens.edu>
2020-08-07 16:09:04 -0400

    Adding total score to answer results by mapping

--- flutterBasics/lib/main.dart
+++ flutterBasics/lib/main.dart
@@ -22,21 +22,39 @@ class _MyAppState extends State<MyApp> {
   static const _questions = [
     {
       'questionText': 'What\'s your favorite color?',
-      'answers': ['Black', 'Red', 'Green', 'White']
+      'answers': [
+        {'text': 'Black', 'score': 2},
+        {'text': 'Red', 'score': 5},
+        {'text': 'Green', 'score': 4},
+        {'text': 'White', 'score': 8}
+      ]
     },
     {
       'questionText': 'What\'s your favorite animal?',
-      'answers': ['Rabbit', 'Snake', 'Elephant', 'Lion']
+      'answers': [
+        {'text': 'Rabbit', 'score': 10},
+        {'text': 'Snake', 'score': 5},
+        {'text': 'Elephant', 'score': 6},
+        {'text': 'Lion', 'score': 2}
+      ]
     },
     {
       'questionText': 'What\'s your favorite car?',
-      'answers': ['Volvo', 'BMW', 'Mercedes-Benz', 'Jeep']
+      'answers': [
+        {'text': 'Volvo', 'score': 10},
+        {'text': 'BMW', 'score': 3},
+        {'text': 'Mercedes-Benz', 'score': 5},
+        {'text': 'Jeep', 'score': 9}
+      ]
     }
   ];
 
   var _questionIndex = 0;
+  var _totalScore = 0;
 
-  void _answerQuestion() {
+  void _answerQuestion(int score) {
+    _totalScore += score;
+
     setState(() {
       _questionIndex = _questionIndex + 1;
     });

8b85dadf6056ee0bc7315ffdc9f5282ff6838f3b
lmcevoy <lmcevoy@stevens.edu>
2020-08-08 12:26:10 -0400

    Computing test score using getters

--- flutterBasics/lib/main.dart
+++ flutterBasics/lib/main.dart
@@ -80,7 +80,7 @@ class _MyAppState extends State<MyApp> {
                 questionIndex: _questionIndex,
                 questions: _questions,
               )
-            : Result(),
+            : Result(_totalScore),
       ),
     );
   }
